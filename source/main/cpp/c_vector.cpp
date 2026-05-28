273a8bed8a731b72f10744d1201f812f93aacf87
Jurgen Kluft2 <jurgen@virtuosgames.com>
2026-05-28 17:59:42 +0800

    * ccontainer
    * vector; fully using an arena

--- source/main/cpp/c_vector.cpp
+++ source/main/cpp/c_vector.cpp
@@ -1,11 +1,71 @@
 #include "ccore/c_target.h"
-#include "ccore/c_limits.h"
 #include "ccore/c_arena.h"
+#include "ccore/c_limits.h"
+#include "ccore/c_memory.h"
 
 #include "ccontainer/c_vector.h"
 
 namespace ncore
 {
-    
+    bool init_vector(vector_t* vector, u32 item_size, u32 num_items_reserved, u32 num_items_committed)
+    {
+        ASSERT(item_size > 0 && item_size <= 65536);  // item size should be reasonable
+        ASSERT(num_items_reserved > 0);               // must reserve at least one item
+
+        vector->m_count  = 0;
+        vector->m_sizeof = item_size;
+        vector->m_arena  = narena::new_arena((int_t)item_size * num_items_reserved, (int_t)item_size * num_items_committed);
+        return vector->m_arena != nullptr;
+    }
+
+    void destroy_vector(vector_t* vector)
+    {
+        if (vector->m_arena != nullptr)
+            narena::destroy(vector->m_arena);
+    }
+
+    bool vector_set_capacity(vector_t* vector, u32 new_capacity)
+    {
+        if (new_capacity == 0 || new_capacity > (D_U32_MAX / vector->m_sizeof))
+            return false;  // new capacity is too large
+
+        vector->m_capacity = new_capacity;
+        return narena::commit(vector->m_arena, (int_t)new_capacity * vector->m_sizeof);
+    }
+
+    u32 vector_get_capacity(vector_t* vector) { return vector->m_capacity; }
+
+    bool push_item(vector_t* vector, byte* item)
+    {
+        byte* dst = (byte*)narena::alloc(vector->m_arena, vector->m_sizeof);
+        g_memcpy(dst, item, vector->m_sizeof);
+        vector->m_count += 1;
+        return true;
+    }
+
+    bool insert_item(vector_t* vector, u32 index, byte* item)
+    {
+        // insert does need to alloc a new slot
+        narena::alloc(vector->m_arena, vector->m_sizeof);
+
+        // if we are inserting at the end, just copy the item to the new slot
+        if (index == vector->m_count)
+        {
+            byte* dst = (byte*)narena::base_ptr(vector->m_arena) + (index * vector->m_sizeof);
+            g_memcpy(dst, item, vector->m_sizeof);
+            vector->m_count += 1;
+            return true;
+        }
+
+        // if we are inserting in the middle, we need to move the existing items after the index
+        // to make room for the new item
+        byte* dst       = (byte*)narena::base_ptr(vector->m_arena) + (index * vector->m_sizeof);
+        byte* src       = dst;
+        u32   move_size = (vector->m_count - index) * vector->m_sizeof;
+        g_memmove(dst + vector->m_sizeof, src, move_size);
+        g_memcpy(dst, item, vector->m_sizeof);
+        vector->m_count += 1;
+        return true;
+    }
 
 };  // namespace ncore
--- source/main/include/ccontainer/c_vector.h
+++ source/main/include/ccontainer/c_vector.h
@@ -11,30 +11,32 @@ namespace ncore
 
     struct vector_t
     {
-        s32      m_count;
-        s32      m_sizeof;
+        u32      m_count;
+        u32      m_sizeof;
+        u32      m_capacity;
         arena_t* m_arena;
     };
 
-    bool init_vector(vector_t& vector, s32 item_size, s32 num_items_reserved, s32 num_items_committed = 0);
-    void destroy_vector(vector_t& vector);
-    bool set_capacity(vector_t& vector, s32 new_capacity);
-    s32  get_reserved(vector_t& vector, s32 item_size);
+    bool vector_setup(vector_t* vector, u32 item_size, u32 num_items_reserved, u32 num_items_committed = 0);
+    void vector_destroy(vector_t* vector);
 
-    bool push_item(vector_t& vector, byte* item);
-    bool insert_item(vector_t& vector, s32 index, byte* item);
+    bool vector_set_capacity(vector_t* vector, u32 new_capacity); // unit = number of items
+    u32  vector_get_capacity(vector_t* vector);
 
-    bool set_item(vector_t& vector, u32 index, const byte* item);
-    bool pop_item(vector_t& vector, byte* out_item);
-    void remove_item(vector_t& vector, u32 index);
-    void swap_remove(vector_t& vector, u32 index);
+    bool push_item(vector_t* vector, byte* item);
+    bool insert_item(vector_t* vector, u32 index, byte* item);
 
-    byte*       get_item_ptr(vector_t& vector, u32 index);
-    byte const* get_item_const_ptr(vector_t& vector, u32 index);
-    byte*       get_items_ptr(vector_t& vector);
-    byte const* get_items_const_ptr(vector_t& vector);
+    bool set_item(vector_t* vector, u32 index, const byte* item);
+    bool pop_item(vector_t* vector, byte* out_item);
+    void remove_item(vector_t* vector, u32 index);
+    void swap_remove(vector_t* vector, u32 index);
 
-    s32 compare_items(vector_t& vector, u32 lhs_index, u32 rhs_index);
+    byte*       get_item_ptr(vector_t* vector, u32 index);
+    byte const* get_item_const_ptr(vector_t* vector, u32 index);
+    byte*       get_items_ptr(vector_t* vector);
+    byte const* get_items_const_ptr(vector_t* vector);
+
+    s32 compare_items(vector_t* vector, u32 lhs_index, u32 rhs_index);
 
 };  // namespace ncore
 
